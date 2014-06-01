7a9555296dd3ecd5ed886806e15102edc89013ac
Zora <MrZoraman@Gmail.com>
2014-06-01 15:49:57 -0700

    point save command added

--- src/com/mrz/dyndns/server/MagicCompass/MagicCompass.java
+++ src/com/mrz/dyndns/server/MagicCompass/MagicCompass.java
@@ -2,22 +2,23 @@ package com.mrz.dyndns.server.MagicCompass;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.mrz.dyndns.server.MagicCompass.commands.SavePointCommand;
 import com.mrz.dyndns.server.MagicCompass.utils.ConfigAccessor;
 import com.mrz.dyndns.server.MagicCompass.zorascommandsystem.bukkitcompat.BukkitCommandSystem;
 
 public class MagicCompass extends JavaPlugin
 {
-	private PointManager pointManager;
-	
 	@Override
 	public void onEnable()
 	{
 		ConfigAccessor pointConfig = new ConfigAccessor(this, "points.yml");
 		
-		pointManager = new PointManager(pointConfig, getLogger());
+		PointManager pointManager = new PointManager(pointConfig, getLogger());
 		pointManager.convertToUuids();
 		
 		BukkitCommandSystem cs = new BukkitCommandSystem(this);
+		
+		cs.registerCommand("point save", new SavePointCommand(pointManager));
 		
 		//point
 		//point set
---
+++ src/com/mrz/dyndns/server/MagicCompass/commands/CommandBase.java
@@ -0,0 +1,19 @@
+package com.mrz.dyndns.server.MagicCompass.commands;
+
+import com.mrz.dyndns.server.MagicCompass.PointManager;
+import com.mrz.dyndns.server.MagicCompass.zorascommandsystem.bukkitcompat.CSBukkitCommand;
+
+public abstract class CommandBase implements CSBukkitCommand
+{
+	private final PointManager pointManager;
+	
+	public CommandBase(PointManager pointManager)
+	{
+		this.pointManager = pointManager;
+	}
+	
+	protected PointManager getPointManager()
+	{
+		return pointManager;
+	}
+}
